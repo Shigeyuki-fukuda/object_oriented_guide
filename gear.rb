32ac2a87cb98651b083a36a67de23d925bbe9645
Shigeyuki-fukuda <urbansonnet1990@gmail.com>
2019-06-08 21:10:04 +0900

    ギアインチの仕組みを使ってGearクラスで車輪の違いを考慮した計算を行えるようにした

--- gear.rb
+++ gear.rb
@@ -13,4 +13,14 @@ class Gear
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    # タイヤはリムの周りを囲むので直径を計算するためには2倍する
+    ratio * (rim + (tire * 2))
+  end
 end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+# -> 137.0909090909091
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+# -> 125.27272727272728
